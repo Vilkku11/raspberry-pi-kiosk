54075cb543a91f28f0c18627a88df3631c6f51bb
William Reima <william.reima@gmail.com>
2024-04-27 18:51:28 +0300

    removed unnecessary stuff

--- setup.sh
+++ setup.sh
@@ -1,12 +1,10 @@
 
 
 DIR=$PWD
-echo $DIR
 
-#echo $USER
-echo $SUDO_USER
+
+
 # Copy necessary files
-
 cp $DIR/script/autostart /home/$SUDO_USER/.config/lxsession/LXDE-pi/autostart
 cp $DIR/script/start.sh /home/$SUDO_USER/.start.sh
 chmod +x /home/$SUDO_USER/.start.sh
