9c07d5cb61bef2d775e267df4798093e680c99e0
William Reima <william.reima@gmail.com>
2024-04-27 18:51:30 +0300

    login to netbox works

--- script/start.sh
+++ script/start.sh
@@ -3,4 +3,20 @@
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/$SUDO_USER/.config/chromium/Default/Preferences
 
 # Start Chromium browser
-/usr/bin/chromium --noerrordialogs --kiosk https://exposure.shodan.io/#/FI
+set -m
+/usr/bin/chromium-browser --noerrordialogs --kiosk https://demo.netbox.dev/login &
+
+
+
+  sleep 20
+
+  xdotool type raspberrypi6
+  sleep 1
+  xdotool key Tab
+  sleep 1
+  xdotool type raspberrypi6
+  sleep 2
+  xdotool key Tab
+  sleep 1
+ xdotool key Return
+
